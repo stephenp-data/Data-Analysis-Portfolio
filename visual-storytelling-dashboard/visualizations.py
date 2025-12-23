c9654c402487d0ba7868b52dc884e579b8d3bfda
stephenp-data <stephenparsonjr@gmail.com>
2025-12-23 16:40:47 -0500

    Update visualizations.py

--- visual-storytelling-dashboard/visualizations.py
+++ visual-storytelling-dashboard/visualizations.py
@@ -1,1 +1,13 @@
+import seaborn as sns
+
+region_sales = (
+    df.groupby('Region')['Sales']
+    .sum()
+    .sort_values(ascending=False)
+)
+
+sns.barplot(
+    x=region_sales.values,
+    y=region_sales.index
+)
 
