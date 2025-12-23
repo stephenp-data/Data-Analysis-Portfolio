e5092183ce8d4a75d78213b9e3964977fd3fff0e
stephenp-data <stephenparsonjr@gmail.com>
2025-12-23 16:56:43 -0500

    Update churn_analysis.py

--- customer-churn-case-study/churn_analysis.py
+++ customer-churn-case-study/churn_analysis.py
@@ -1,5 +1,17 @@
+import pandas as pd
+
+"""
+Purpose:
+Analyze churn rate by contract type to identify high-risk customer segments.
+"""
+
+df = pd.read_csv("data/telco_churn.csv")
+
 churn_by_contract = (
     df.groupby('Contract')['Churn']
     .apply(lambda x: (x == 'Yes').mean() * 100)
 )
+
+print(churn_by_contract)
+
 
