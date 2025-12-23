0d3e290e7b205b90266e84f76440258f4e2f088e
stephenp-data <stephenparsonjr@gmail.com>
2025-12-23 16:36:03 -0500

    Update data_cleaning.py

--- end-to-end-business-analysis/data_cleaning.py
+++ end-to-end-business-analysis/data_cleaning.py
@@ -1,1 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv("data/raw/online_retail.csv")
+
+# Remove canceled orders
+df = df[~df['InvoiceNo'].astype(str).str.startswith('C')]
+
+# Handle missing customers
+df = df.dropna(subset=['CustomerID'])
+
+# Create revenue metric
+df['Revenue'] = df['Quantity'] * df['UnitPrice']
+
+# Convert date
+df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+
+df.to_csv("data/cleaned/cleaned_sales.csv", index=False)
 
