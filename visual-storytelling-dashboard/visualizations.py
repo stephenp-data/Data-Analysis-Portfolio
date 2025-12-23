82f0514a09322d61024a80e2a7d6d035a3c24884
stephenp-data <stephenparsonjr@gmail.com>
2025-12-23 17:22:18 -0500

    Update visualizations.py

--- visual-storytelling-dashboard/visualizations.py
+++ visual-storytelling-dashboard/visualizations.py
@@ -1,4 +1,14 @@
+import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+
+"""
+Purpose:
+Visualize regional sales performance to support executive-level
+decision making and storytelling.
+"""
+
+df = pd.read_csv("data/global_superstore.csv")
 
 region_sales = (
     df.groupby('Region')['Sales']
@@ -11,3 +21,8 @@ sns.barplot(
     y=region_sales.index
 )
 
+plt.title("Total Sales by Region")
+plt.xlabel("Sales")
+plt.ylabel("Region")
+plt.tight_layout()
+plt.show()
